8a76ae4077be203a56da3ca77801ed0e99c376a7
tennis830531-byte <tennis830531@gmail.com>
2026-07-29 15:32:08 +0800

    Configure production Turnstile site key

--- supabase-config.js
+++ supabase-config.js
@@ -2,3 +2,4 @@ window.CHROMATICA_SUPABASE_CONFIG = Object.freeze({
   url: "https://ljykxwqmjguybywyatrm.supabase.co",
   publishableKey: "sb_publishable_tKOV6uG6d6bibPRjvZur7A_fTWidAE1",
 });
+window.CHROMATICA_TURNSTILE_SITE_KEY = "0x4AAAAAAEAo5qF5ZmpB-jdJ";
